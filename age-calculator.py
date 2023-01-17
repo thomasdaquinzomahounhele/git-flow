d81e069a5eb1bb6cbbf980790793eedf264159d6
thomasdaquin <thomasdaquinzomahoun@gmail.com>
2023-01-17 14:32:39 +0100

    new update
    added age and year

--- age-calculator.py
+++ age-calculator.py
@@ -1,2 +1,4 @@
 name = input("What is your name: ")
-print(name + " will be 100 years old in the year 2080")
+age = int(input("How old are you: "))
+year = str((2018 - age)+100)
+print(name + " will be 100 years old in the year " + year)
